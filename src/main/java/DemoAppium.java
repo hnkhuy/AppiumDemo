509570e2c8821c6f3fef8700c6b57e7e274e4eb4
HuyHuynhZ <huy.huynh@titancorpvn.com>
2018-09-27 14:33:04 +0700

    change to Android Driver

--- src/main/java/DemoAppium.java
+++ src/main/java/DemoAppium.java
@@ -1,8 +1,7 @@
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,19 +15,22 @@ import java.net.URL;
  */
 
 public class DemoAppium {
-    WebDriver driver;
+    private AndroidDriver<WebElement> driver;
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("device", "Android");
-        capabilities.setCapability("deviceName", "Android");
         capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Android");
         capabilities.setCapability("VERSION", "7.0");
+        //        capabilities.setCapability("deviceName", "Galaxy S8");
+        //        capabilities.setCapability("VERSION", "8.0");
         capabilities
                 .setCapability("app", System.getProperty("user.dir") + "\\src\\main\\resources\\apk\\DemoAppium.apk");
+        //        capabilities.setCapability("noReset", true);
 
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @Test
